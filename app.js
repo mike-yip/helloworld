73382cf1929485071e89eabbc5c6ac42bba52923
mike_yip706 <mike_yip706@hotmail.com>
2022-02-07 00:29:08 -0800

    Test

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.send('Not found!!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
